863b8bd9ac3a6215e32140e9e4ba55f116940d6d
Amer Kumar <amer.tanwani@hotmail.com>
2018-09-25 05:06:50 +0500

    Add notification

--- app/src/main/java/app/mapin/amerkumar/regionsdetection/RegionsActivity.java
+++ app/src/main/java/app/mapin/amerkumar/regionsdetection/RegionsActivity.java
@@ -1,9 +1,14 @@
 package app.mapin.amerkumar.regionsdetection;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -17,6 +22,9 @@ import android.widget.TextView;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
@@ -25,12 +33,19 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class RegionsActivity extends AppCompatActivity {
 
-    private static final String TAG = "app.mapin.RegionActivity";
+    private static final String TAG = "ap.mapin.RegionActivity";
     private static final int REQUEST_CHECK_SETTINGS = 0;
+    private static final String GEOFENCE_REQ_ID = "GIK Incubator";
+    private static final String PRIMARY_CHANNEL_ID = "Mascot Notification";
 
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
@@ -46,6 +61,13 @@ public class RegionsActivity extends AppCompatActivity {
     private TextView mAccuracyTextView;
     private TextView mAltitudeTextView;
     private LocationCallback mLocationCallback;
+    private GeofencingClient mGeofencingClient;
+    private List<Geofence> mGeofenceList = new LinkedList<>();
+    private double INCUBATOR_LATITUDE = 34.071704;
+    private double INCUBATOR_LONGITUDE = 72.645717;
+    private float INCUBATOR_GEOFENCE_RADIUS_IN_METERS = 30;
+    private PendingIntent mGeofencePendingIntent;
+    private NotificationManager mNotifyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,16 +75,82 @@ public class RegionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_regions);
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mGeofencingClient = LocationServices.getGeofencingClient(this);
         requestLocationPermissions();
         requestGPSPermissions();
         getLocationUpdates();
-
+        addGeofences();
+        geoFencingInit();
 
         mLatitudeTextView = (TextView) findViewById(R.id.latitude);
         mLongitudeTextView = (TextView) findViewById(R.id.longitude);
         mAccuracyTextView = (TextView) findViewById(R.id.accuracy);
         mAltitudeTextView = (TextView) findViewById(R.id.altitude);
 
+    }
+
+    private void addGeofences() {
+        mGeofenceList.add( new Geofence.Builder()
+                .setRequestId(GEOFENCE_REQ_ID)
+                .setCircularRegion(INCUBATOR_LATITUDE,
+                        INCUBATOR_LONGITUDE,
+                        INCUBATOR_GEOFENCE_RADIUS_IN_METERS)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+                .setExpirationDuration(10000)
+                .build());
+    }
+
+    private void geoFencingInit() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Log.d(TAG, "geofence init");
+        mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+//                        Geofences added
+                        Log.d(TAG, "Added GeoFence Successful");
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+//                        failure to add geofences
+                        Log.d(TAG, "Geofence failure");
+                    }
+                });
+    }
+
+
+    private GeofencingRequest getGeofencingRequest() {
+
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
     }
 
     private void requestGPSPermissions() {
@@ -180,7 +268,7 @@ public class RegionsActivity extends AppCompatActivity {
                 Log.d(TAG, "Get Location");
                 if (locationResult == null)
                     return;
-                Log.d(TAG, "size "  + locationResult.getLocations().size() + "");
+                Log.d(TAG, "size " + locationResult.getLocations().size() + "");
                 for (Location location : locationResult.getLocations()) {
                     String latitude = mLatitudeTextView.getText().toString();
                     mLatitudeTextView.setText(latitude + " " + location.getLatitude());
@@ -204,7 +292,27 @@ public class RegionsActivity extends AppCompatActivity {
     }
 
     private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mFusedLocationProviderClient.requestLocationUpdates(createLocationRequest(),
                 mLocationCallback, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
     }
 }
